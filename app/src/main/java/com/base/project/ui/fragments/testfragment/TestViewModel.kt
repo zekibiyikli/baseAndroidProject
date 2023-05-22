7c69aba92b09d9f796e3db5c66ec1e49b40a6949
Zekeriya Bıyıklı <zbiyikli1@gmail.com>
2023-05-22 14:46:46 +0300

    Added Coroutine

--- app/src/main/java/com/base/project/ui/fragments/testfragment/TestViewModel.kt
+++ app/src/main/java/com/base/project/ui/fragments/testfragment/TestViewModel.kt
@@ -2,6 +2,13 @@ package com.base.project.ui.fragments.testfragment
 
 import com.base.core.base.BaseViewModel
 import com.base.data.repository.MainRepo
+import kotlinx.coroutines.launch
 
 class TestViewModel : BaseViewModel<MainRepo>(MainRepo::class) {
+
+    fun testCoroutine(){
+        launch {
+            // Work on async
+        }
+    }
 }
--- core/src/main/java/com/base/core/base/BaseViewModel.kt
+++ core/src/main/java/com/base/core/base/BaseViewModel.kt
@@ -1,8 +1,20 @@
 package com.base.core.base
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 import kotlin.reflect.KClass
 
-abstract class BaseViewModel<out REPO : BaseRepository>(repoType: KClass<REPO>) : ViewModel() {
+abstract class BaseViewModel<out REPO : BaseRepository>(repoType: KClass<REPO>) : ViewModel(),CoroutineScope {
 
+    private val job= Job()
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
 }
